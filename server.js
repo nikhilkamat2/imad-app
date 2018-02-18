31200d31beac45e712fd6e94340ba8f8033e10e1
Nikhil Kamat <nikhilkamat2@gmail.com>
2018-02-18 16:26:55 +0530

    Changed to Server to include pages

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/profile', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'profile.html'));
+});
+
+app.get('/poem', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'poem.html'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
