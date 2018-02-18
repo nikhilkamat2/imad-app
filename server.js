00a801b6abb1aa721e29ebe93e90133a90047aab
Nikhil Kamat <nikhilkamat2@gmail.com>
2018-02-18 20:50:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ function createTemplate (data) {
        <body>
             <div class="container">
                 <div>
-                    <a href='\'>Home</a>
+                    <a href='/' >Home</a>
                 </div>
                 <hr>
                 <h3>
