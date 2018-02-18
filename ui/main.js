dfb92f2eee4d9914080fbc9ca9dd67c79d8fffc6
Nikhil Kamat <nikhilkamat2@gmail.com>
2018-02-18 22:26:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,13 @@
-console.log('Loaded!');
-var element=document.getElementById('maintext');
-element.innerHTML='IMAD WebApp';
-
-var img = document.getElementById('img');
-
-var marginLeft=0;
-function moveRight() {
-	marginLeft = marginLeft + 1;
-	img.style.marginLeft = marginLeft+'px';
-}
-
-img.onclick= function() {
-	var interval = setInterval(moveRight,50);
-
+//Counter 
+var button = button.document.getElementById("counter");
+var counter = 0;
+button.onclick = function() {
+  // Make a request to the counter endpoinnt
+  
+  // Capture the response and store it in a variable
+  
+  // Render the variable in the correct span
+  counter = counter + 1;
+  var span = document.getElementById('count');
+  span.innerHTML=counter.toString();
 };
-
